aab634ee38254bede841c2e7b0e6b539f733e191
Dren Kajmakci <dk18184@gmail.com>
2016-11-06 19:13:08 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -30,7 +30,7 @@
         
         var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
     
-        for(var i=0; i < length; i++)
+        for(var i = 0; i < length; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));
     
         return text;
